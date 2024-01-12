7d9ea42af547fe420678d54324f4aea865f75df5
Daniel J Moore <djm1176@live.missouristate.edu>
2024-01-12 12:46:59 -0600

    Implemented Singleton pattern

---
+++ 03_SingletonPattern/GlobalProgramState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Represents the state of the program during execution. There should only ever be 1 instance.
+/// </summary>
+internal class GlobalProgramState {
+
+	// Define the singleton
+	private static GlobalProgramState? instance = null;
+	
+	// Provide a public property to access the singleton, or create it on first access
+	public static GlobalProgramState Instance { get {
+			if (instance == null) {
+				instance = new GlobalProgramState();
+			}
+
+			return instance;
+		}
+	}
+
+	// Class members
+	public DateTime StartupTime { get; private set; } = default;
+
+
+	// Disallow creation of the singleton class outside of itself
+	private GlobalProgramState () {
+		StartupTime = DateTime.Now;
+	}
+
+}
---
+++ 03_SingletonPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program {
+	public static void Main (string[] args) {
+		// Get the singleton instance
+		GlobalProgramState instance = GlobalProgramState.Instance;
+
+		// Note that we can't create our own instance of the singleton class
+		// GlobalProgramState instance = new GlobalProgramState();
+
+		Console.WriteLine($"The singleton instance reports startup time of {instance.StartupTime}");
+
+	}
+}
