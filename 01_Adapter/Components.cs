3b9d9b3a009ed1810cfb587cc04a71eb7e385b60
Daniel J Moore <djm1176@live.missouristate.edu>
2024-01-12 15:51:36 -0600

    Implemented Adapter

---
+++ 01_Adapter/Components.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface ILaunchable {
+	public void Launch ();
+}
+
+interface IShootable {
+	public void Shoot ();
+}
+
+class RocketLauncher : ILaunchable {
+	public void Launch () {
+		Console.WriteLine("Rocket Launcher launches!");
+	}
+}
+
+class MachineGun : IShootable {
+	public void Shoot () {
+		Console.WriteLine("Explosive Projectile shoots!");
+	}
+}
+
+
+// Adapts a launching weapon into a shootable one instead
+class RocketLauncherAdapter : IShootable {
+
+	// We will adapt a RocketLauncherAdapter instance, which is an IShootable, into a RocketLauncher
+	RocketLauncher launcher;
+
+	public RocketLauncherAdapter (RocketLauncher launcher) {
+		this.launcher = launcher;
+	}
+
+	public void Shoot () {
+		launcher.Launch();
+	}
+}
---
+++ 01_Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program {
+	public static void Main (string[] args) {
+		// Demonstrate the ILaunchable
+		RocketLauncher launcher = new RocketLauncher();
+		Console.WriteLine("Rocket Launcher (ILaunchable):");
+		launcher.Launch();
+
+		// Demonstrate the IShootable
+		MachineGun gun = new MachineGun();
+		Console.WriteLine("Machine Gun (IShootable):");
+		gun.Shoot();
+
+		// Demonstrate the Adapter from IShootable to ILaunchable
+		RocketLauncherAdapter adapter = new RocketLauncherAdapter(launcher);
+		Console.WriteLine("Rocket Launcher Adapter (IShootable):");
+		adapter.Shoot();
+	}
+}
